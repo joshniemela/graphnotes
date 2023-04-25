5a8b919cf5c725724b31f4de3db1464367fad3bf
Joshua Niemelä <josh@jniemela.dk>
2023-04-25 13:17:57 +0200

    Added todo

---
+++ TODO.md
@@ -0,0 +1,42 @@
+# TODO
+
+## Schema
+
+
+## API things
+* Add node / Add label
+* Add relations
+* Add field / Set property
+* Upload file to property
+* Create delete request / Hide 
+
+## CLI / TUI(?)
+
+### STEP BY STEP TUI
+#### Add node
+    * Prompt name (full-text search)
+    * Prompt label(s) 
+    * Prompt properties / Add note reference
+    * Prompt relation(s)(?)
+
+#### Add relation(s)
+    * Prompt from => to
+    * Type
+    * label(s?) if neccesary
+
+#### Delete node
+* Prompt name (DETACH DELETE or mark for deletion)
+
+#### Delete relation
+* Prompt from to
+
+#### Modify node
+* prompt name
+* prompt property to modify or labels
+* prompt new data
+
+#### Modify relation
+* prompt from to
+* prompt property to modify or labels
+* prompt new data
+
--- src/app/core.clj
+++ src/app/core.clj
@@ -55,17 +55,66 @@ Options:
     :ref-id "p"
     :labels [:Course]
   }))
-    
+  
+(defn add-elem [conn name]
+  (client/create-node! conn {
+    :ref-id "n"
+    :labels [:Element]
+    :props {:name name}
+  }))
+
+(defn add-elems [conn names]
+  (doseq [name names]
+    (add-elem conn name)))
+
+
+
+(defn add-curr-edge [conn from to]
+  (client/create-rel! conn {
+    :ref-id "r"
+    :type "Curriculum"
+    :from {:labels [:Course] :props {:name from}}
+    :to {:labels [:Element] :props {:name to}}
+  }))
+
+(defn add-curr-nodes [conn course names]
+  (doseq [name names]
+    (add-elem conn name))
+  (doseq [name names]
+    (add-curr-edge conn course name)))
+
+;; Macro should add transaction to all calls
+;;(transaction conn
+;;  (client/create-node! {:ref-id "p" :labels [:person] :props {:first-name "Neo"}})
+;;  (client/create-node! {:ref-id "p" :labels [:person] :props {:first-name "Morpheus"}}))
+;; =>
+;; (client/with-transaction conn transac
+;;  (client/create-node! trasac {:ref-id "p" :labels [:person] :props {:first-name "Neo"}})
+;;  (client/create-node! transac {:ref-id "p" :labels [:person] :props {:first-name "Morpheus"}}))
+
+(defn insertSecond [sexpr x]
+  ; insert at second position
+  (cons (first sexpr) (cons x (rest sexpr))))
+
+(defmacro transaction [conn & body]
+  ; Add client/with-transaction to body
+  (let [x (gensym)]
+    `(client/with-transaction ~conn ~x
+      ; insert x as first argument to all body calls
+      ~@(map #(insertSecond % x) body))))
+      
+  
+
+
 (defn -main [& args]
   (let [args (parse-opts args cli-options)]
     (let [opts (get args :options)]
       (check-if-help opts)
       (assert-login opts)
       (let [connection (connect opts)]
-        (println (try
-          (query-courses connection)
-          (catch Exception e
-            (println "Error: " (.getMessage e))
-            (System/exit 1))))
+        ;; Somehow get exception happening inside of transaction
+        (add-curr-nodes connection "TestCourse" ["TestElem1" "TestElem2"])
+        
+        
         (client/disconnect connection)))))
     
